94d9a6da962dd4ef00a40285ddcc9fae84f6a2c7
Khrystyna Peryk <kristik991@gmail.com>
2025-02-26 13:50:58 +0400

    summary improve

--- src/components/SummaryBox.jsx
+++ src/components/SummaryBox.jsx
@@ -1,12 +1,14 @@
 import "./SummaryBox.css"
 
 const SummaryBox = ({dataList, startOver}) => {
-
+console.log(dataList)
     return (
         <div className="summary-container">
             <div className="summary-box">
                 <h3>Your Selection:</h3>
-                <p>{dataList.join(", ")}</p>
+                <ul className="summary-list">
+                    {dataList.map((item, index) => <li className="summary-item" key={index}>{item}</li>)}
+                </ul>
                 <button type="button" onClick={() => startOver()} className="start-over-btn">Start Over</button>
             </div>
         </div>
